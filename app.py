6a56217dc6d5dc3d537e04584d7551737bf90561
Jhostyn <jjgavilanez@espe.edu.ec>
2022-07-14 22:25:59 -0500

    Creacion de documento e inserccion de un nuevo login - script

--- app.py
+++ app.py
@@ -18,4 +18,17 @@ connect(MONGO_BASEDATOS)
 
 print(baseDatos.list_collection_names())
 
+#Definicion de esquema rigido mongodb con una clase
+class logins(Document):
+    ID_paralelo = StringField(required=True)
+    contrasenia = StringField(required=True, min_length=8, max_length=30)
+    
+#Crear documento
+loginParalelo1 = logins(
+    ID_paralelo = "ObjectId('78sfd0889er08')",
+    contrasenia = "contraseñaParaleloA"
+)
+loginParalelo1.save() #Insertar un nuevo login
 
+for doc in logins.objects:
+ print(doc.ID_paralelo)
